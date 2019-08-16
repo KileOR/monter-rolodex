46e5ccdcf610203ff7b250100755200acda7f775
Assanali Issatayev <assanali@rocketfirm.com>
2019-08-16 18:54:46 +0600

    Arrow functions and searchbox component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component'; 
 
 import './App.css';
 
@@ -12,12 +13,17 @@ class App extends Component {
       monsters: [],
       searchField: ''
     }
+
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
   }
 
   render() {
@@ -27,7 +33,7 @@ class App extends Component {
       );
     return (
       <div className="App">
-        <input type='search' placeholder='Search monsters' onChange={e => this.setState({ searchField: e.target.value })}/>
+        <SearchBox placeholder='Search Monster' handleChange={this.handleChange}/>
         <CardList monsters={filtredMonsters} />
       </div>
     );
